f845ad6f88e01131f32b49e43ae6be6c9079d493
eunji yang <eunji9128@gmail.com>
2023-05-30 22:29:27 +0900

    fix: add removed lines due to conflict at Tests

--- src/Personal/Tests.jsx
+++ src/Personal/Tests.jsx
@@ -3,6 +3,7 @@ import NoTest from "./NoTest";
 import Test1 from "./Test1";
 import Test2 from "./Test2";
 import Test5 from "./Test5";
+import Test3 from "./Test3";
 
 export const Tests = () => {
     const params = useParams();
@@ -11,6 +12,8 @@ export const Tests = () => {
             return <Test1 />
         case '2':
             return <Test2 />
+        case '3':
+            return <Test3 />
         case '5':
             return <Test5 />    
         default:
